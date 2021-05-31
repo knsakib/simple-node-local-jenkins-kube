4a646a64a6e55abcb411b94a3b06fb12581322bd
Khalid Nazmus Sakib <knsakib@C02CR2MRMD6P@bnc.ca>
2021-05-31 18:08:08 -0400

    update the message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello, This is running from local Kubernetes Cluster!')
 })
 
 app.listen(port, () => {
